5440eb0787aa58ad92d7e0026a437e4224549428
root <root@vmi418798.contaboserver.net>
2021-02-17 08:10:24 +0100

    update

--- application/config/facebook.php
+++
@@ -1,6 +0,0 @@
-<?php
-defined('BASEPATH') OR exit('No direct script access allowed');
-
-$config['facebook_api_id']   = '1518841958266580';
-$config['facebook_app_secret']  = 'e6dfe208e69fbe6f8d4928936a81c593';
-?>
